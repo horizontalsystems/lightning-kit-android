3a55b1bd26bb39cc93881ce58504c16f623fb7a0
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-02-24 10:28:34 +0600

    Make the RemoteLndCredentials as Parcelable

--- lightningkit/src/main/java/io/horizontalsystems/lightningkit/remote/RemoteLndCredentials.kt
+++ lightningkit/src/main/java/io/horizontalsystems/lightningkit/remote/RemoteLndCredentials.kt
@@ -1,3 +1,12 @@
 package io.horizontalsystems.lightningkit.remote
 
-data class RemoteLndCredentials(val host: String, val port: Int, val certificate: String, val macaroon: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class RemoteLndCredentials(
+        val host: String,
+        val port: Int,
+        val certificate: String,
+        val macaroon: String
+) : Parcelable
